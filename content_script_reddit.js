5afbd6b3010617a4b51b4182831eca113c98b690
Aditya <adityanivarthi19@gmail.com>
2014-12-06 14:23:05 -0500

    Added reddit subreddit replacement and reddit comments replacement.

--- content_script_reddit.js
+++ content_script_reddit.js
@@ -1,2 +1,36 @@
-console.log(getTextSample(4));
-console.log(getTextSample(4));
+function subRedditReplacement() {
+	var childrenList = document.getElementById("siteTable").children;
+
+	for (var i = 0; i < childrenList.length; i++) {
+		var childList = childrenList[i].children;
+		for (var j = 0; j < childList.length; j++) {
+			if ((/(^|\s)entry($|\s)/).test(childList[j].className)) {
+				var aChildList = childList[j].getElementsByTagName("a");
+				for (var k = 0; k < aChildList.length; k++) {
+					if ((/(^|\s)title($|\s)/).test(aChildList[k].className)) {
+						aChildList[k].innerText = getTextSample(1);;
+					}
+				}
+			}
+		}
+	}
+}
+
+function commentReplacement() {
+	var childrenList = document.getElementsByTagName("div");
+
+	for (var i = 0; i < childrenList.length; i++) {
+		if ((/(^|\s)entry($|\s)/).test(childrenList[i].className)) {
+			var childList = childrenList[i].getElementsByClassName("md");
+			for (var j = 0; j < childList.length; j++) {
+				childList[j].innerText = getTextSample(1);
+			}
+		}
+	}
+}
+
+if ((/http:\/\/www\.reddit\.com\/r\/.+?\/comments\/.+\//).test(document.URL)) {
+	commentReplacement();
+} else {
+	subRedditReplacement();
+}
