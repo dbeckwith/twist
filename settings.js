a6d920bb91b251f4c96b33c8345e5786a733be52
Daniel Beckwith <djbsnx@gmail.com>
2014-12-06 01:35:21 -0500

    Settings saving and loading work

--- settings.js
+++ settings.js
@@ -1,15 +1,24 @@
 function saveText() {
     var knowledge = document.getElementById("knowledgeTextArea").value;
-    alert("asdkhfaskdjfh");
     chrome.storage.sync.set({
-        "knowledgeText": knowledge
+        knowledgeText: knowledge
     }, function() {
-        alert("hello");
+        alert("options saved");
         document.getElementById("saveStatus").innerHTML = "Options Saved!";
         setTimeout(function() { document.getElementById("saveStatus").innerHTML = ""; }, 1000);
     });
 }
 
+function loadText() {
+    chrome.storage.sync.get({
+        knowledgeText: "[none]"
+    }, function(data) {
+        alert("options loaded: " + data.knowledgeText);
+        document.getElementById("saveStatus").innerHTML = data.knowledgeText;
+    });
+}
+
 window.onload = function() {
     document.getElementById("submitButton").onclick = saveText;
+    document.getElementById("loadButton").onclick = loadText;
 }
