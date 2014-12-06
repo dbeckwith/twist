119ade54ddebea53e1893a7b58a09145b013a4f6
Daniel Beckwith <djbsnx@gmail.com>
2014-12-06 14:44:26 -0500

    Twitter almost working. Also got rid of run_at field in content_scripts.

--- content_script_twitter.js
+++ content_script_twitter.js
@@ -1,6 +1,12 @@
 console.log("Running TWIST on twitter");
 
-var tweets = document.getElementsByClassName("ProfileTweet-text");
+
+if (/^https:\/\/twitter\.com\/(i\/discover)?$/.test(document.URL)) {
+    var tweets = document.getElementsByClassName("js-tweet-text tweet-text");
+}
+else {
+    var tweets = document.getElementsByClassName("ProfileTweet-text");
+}
 
 for (var i = 0; i < tweets.length; i++) {
     tweets[i].innerText = getTextSample(3);
