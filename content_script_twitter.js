be7e0d4e731f8374ba13c0693c055e69484e38de
Daniel Beckwith <djbsnx@gmail.com>
2014-12-06 15:15:30 -0500

    Fixed text sample bugs.

--- content_script_twitter.js
+++ content_script_twitter.js
@@ -1,13 +1,15 @@
-console.log("Running TWIST on twitter");
+function makeEducational() {
+    console.log("Running TWIST on twitter");
 
 
-if (/^https:\/\/twitter\.com\/(i\/discover)?$/.test(document.URL)) {
-    var tweets = document.getElementsByClassName("js-tweet-text tweet-text");
-}
-else {
-    var tweets = document.getElementsByClassName("ProfileTweet-text");
-}
+    if (/^https:\/\/twitter\.com\/(i\/discover)?$/.test(document.URL)) {
+        var tweets = document.getElementsByClassName("js-tweet-text tweet-text");
+    }
+    else {
+        var tweets = document.getElementsByClassName("ProfileTweet-text");
+    }
 
-for (var i = 0; i < tweets.length; i++) {
-    tweets[i].innerText = getTextSample(3);
+    for (var i = 0; i < tweets.length; i++) if (isUserReadyToBeEducated()) {
+        tweets[i].innerText = getTextSample(3);
+    }
 }
