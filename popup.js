11efa9bbd6ee2cc5d49dc38867c8cb28aee59b7a
chickenbellyfinn <chickenbellyfinn@gmail.com>
2014-12-06 21:30:11 -0500

    slider animation

--- popup.js
+++ popup.js
@@ -7,7 +7,9 @@ window.onload = function() {
         value = Math.floor(value.study_chance*100);
     	document.getElementById("study_slider").value = value;
     	document.getElementById("percent_label").innerHTML = value+"%";
-        document.getElementById("logo-img").src = "img/logo/logo" + value + ".png";
+    	value = 100-value;
+    	if(value == 100)value = 99; 
+        document.getElementById("logo-img").src = "img/logo/logo" + value + ".jpg";
     });
 
     document.getElementById("study_slider").addEventListener("change", studySliderChanged, false);
@@ -18,5 +20,7 @@ function studySliderChanged() {
 	var value = document.getElementById("study_slider").value;
     chrome.storage.sync.set({"study_chance":value/100},null);
     document.getElementById("percent_label").innerHTML = value+"%";
-    document.getElementById("logo-img").src = "img/logo/logo" + value + ".png";
+    value = 100-value;
+    if(value == 100)value = 99;
+    document.getElementById("logo-img").src = "img/logo/logo" + value + ".jpg";
 }
